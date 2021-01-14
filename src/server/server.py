9ad6a56336ebe68a92f727e9f6954f4a2ca1a2e1
Simon Dale <sj.dale@gmail.com>
2021-01-14 21:45:35 +0000

    Updated server to generate swagger output

--- src/server/server.py
+++ src/server/server.py
@@ -3,10 +3,49 @@ from azureml.contrib.services.aml_request import AMLRequest, rawhttp
 from azureml.contrib.services.aml_response import AMLResponse
 from rpy2.robjects import r, pandas2ri
 from rpy2.rinterface_lib import openrlib
+from inference_schema.schema_decorators import input_schema, output_schema
+from inference_schema.parameter_types.pandas_parameter_type import (
+    PandasParameterType,
+)
 
 import pandas as pd
 import subprocess
 import os
+
+
+class PassThroughPandasParameterType(PandasParameterType):
+    def __init__(self, sample_input):
+        super(PassThroughPandasParameterType, self).__init__(sample_input)
+
+    def deserialize_input(self, input_data):
+        return input_data
+
+
+sample_input = PassThroughPandasParameterType(
+    pd.DataFrame(
+        columns=["sepal_length", "sepal_width", "petal_length", "petal_width"],
+        index=[0, 1],
+        data=[[5.1, 3.5, 1.4, 0.2], [2.9, 1.0, 5.4, 3.2]],
+    )
+)
+
+
+sample_output = PassThroughPandasParameterType(
+    pd.DataFrame(
+        columns=[
+            "sepal_length",
+            "sepal_width",
+            "petal_length",
+            "petal_width",
+            "predicted_species",
+        ],
+        index=[0, 1],
+        data=[
+            [5.1, 3.5, 1.4, 0.2, "setosa"],
+            [2.9, 1.0, 5.4, 3.2, "virginica"],
+        ],
+    )
+)
 
 
 def init():
@@ -17,8 +56,22 @@ def init():
     )
 
     if not os.path.exists("packages_installed"):
-        subprocess.check_call(["R", "-q", "-e", 'install.packages("caret", quiet=TRUE, repos="http://cran.us.r-project.org")'])
-        subprocess.check_call(["R", "-q", "-e", 'install.packages("e1071", quiet=TRUE, repos="http://cran.us.r-project.org")'])    
+        subprocess.check_call(
+            [
+                "R",
+                "-q",
+                "-e",
+                'install.packages("caret", quiet=TRUE, repos="http://cran.us.r-project.org")',
+            ]
+        )
+        subprocess.check_call(
+            [
+                "R",
+                "-q",
+                "-e",
+                'install.packages("e1071", quiet=TRUE, repos="http://cran.us.r-project.org")',
+            ]
+        )
         with open("packages_installed", "w") as f:
             f.writelines([1])
 
@@ -27,11 +80,13 @@ def init():
         model = r.readRDS(model_path)
 
 
+@input_schema("request", sample_input)
+@output_schema(sample_output)
 @rawhttp
 def run(request: AMLRequest) -> AMLResponse:
     if request.method == "GET":
         return AMLResponse("ok", 200)
-    if request.method == "POST":        
+    if request.method == "POST" and request.full_path.startswith("/score"):
         body = request.get_data(False)
         pdf = pd.read_json(body, orient="split")
 
@@ -51,7 +106,7 @@ def run(request: AMLRequest) -> AMLResponse:
         pdf.columns = r_columns
 
         with openrlib.rlock:
-            rdf = pandas2ri.py2rpy(pdf)            
+            rdf = pandas2ri.py2rpy(pdf)
             predictions = r.predict(model, rdf, method="class")
 
         pdf.columns = python_columns
